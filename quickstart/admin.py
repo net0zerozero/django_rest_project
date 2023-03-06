927aa42e8813c4e4f3881b35e7afed2bed500f8a
Net_Zero <netrannetranner@gmail.com>
2023-03-06 15:44:00 +0600

    first commit

---
+++ quickstart/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Category, Product
+
+# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'updated_at')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+    list_filter = ('title',)
+    
+    
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'price', 'category', 'created_at', 'updated_at')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+    list_filter = ('title',)
+    
